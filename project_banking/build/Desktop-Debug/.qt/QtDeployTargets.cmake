100dec98dd7ddf777fe29f5fbb6cee29182dd821
sthaarwin <sthaarwin123@gmail.com>
2024-06-03 13:53:14 +0545

    Please enter the commit message for your changes. Lines starting directory removed

--- project_banking/build/Desktop-Debug/.qt/QtDeployTargets.cmake
+++
@@ -1,2 +0,0 @@
-set(__QT_DEPLOY_TARGET_project_banking_FILE F:/C++/project/Project Banking/Project-Banking/project_banking/build/Desktop-Debug/project_banking.exe)
-set(__QT_DEPLOY_TARGET_project_banking_RUNTIME_DLLS D:/QT/6.7.0/mingw_64/bin/Qt6Widgets.dll;D:/QT/6.7.0/mingw_64/bin/Qt6Gui.dll;D:/QT/6.7.0/mingw_64/bin/Qt6Core.dll)
