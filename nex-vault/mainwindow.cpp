0772ce4073652240553a0d318a20a6eafe23e4af
sthaarwin <sthaarwin123@gmail.com>
2024-06-06 15:00:01 +0545

    added validator

--- nex-vault/mainwindow.cpp
+++ nex-vault/mainwindow.cpp
@@ -1,12 +1,19 @@
 #include "mainwindow.h"
 #include "./ui_mainwindow.h"
+#include <QINTValidator>
+#include <QLineEdit>
+#include <QMessageBox>
 
 MainWindow::MainWindow(QWidget* parent)
     : QMainWindow(parent)
     , ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+
     ui->stackedWidget->setCurrentIndex(0);
+	QIntValidator* intValidator = new QIntValidator(0, 9999999999, this);
+	ui->stackedWidget->findChild<QLineEdit*>("phoneNumberInputLogin")->setValidator(intValidator);
+
     connect(ui->stackedWidget->findChild<QPushButton*>("loginButton"), &QPushButton::clicked, this, &MainWindow::on_loginButton_clicked);
 	connect(ui->stackedWidget->findChild<QPushButton*>("signUp"), &QPushButton::clicked, this, &MainWindow::on_signupButton_clicked);
 }
@@ -18,6 +25,17 @@ MainWindow::~MainWindow()
 
 void MainWindow::on_loginButton_clicked()
 {
+    QString phoneNumberStr = ui->stackedWidget->findChild<QLineEdit*>("phoneNumberInputLogin")->text();
+	long int phoneNumber = phoneNumberStr.toLong();
+    if (phoneNumberStr.length() != 10) {
+        QMessageBox::warning(this, "Invalid Phone Number", "Please enter a valid phone number.");
+        return;
+    }
+    QString password = ui->stackedWidget->findChild<QLineEdit*>("passwordInputLogin")->text();
+    if (password.length() < 8) {
+		QMessageBox::warning(this, "Invalid Password", "Please enter a valid password.");
+		return;
+	}
 	ui->stackedWidget->setCurrentIndex(1);
 }
 
